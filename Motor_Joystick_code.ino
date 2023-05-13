5d3faecc5140d2284e3ea8bddc22ebf414abe849
pfisterhub <133514819+pfisterhub@users.noreply.github.com>
2023-05-13 16:23:22 +0200

    Update Motor_Joystick_code.ino

--- Motor_Joystick_code.ino
+++ Motor_Joystick_code.ino
@@ -1,5 +1,6 @@
+//with this code, you can control 2 motors
 
-
+//motor driver pins connection
 int ena=8;
 int in1=9;
 int in2=10;
@@ -8,6 +9,7 @@ int enb=7;
 int in3=5;
 int in4=6;
 
+//joystick analog pins connections
 int vrx = A0;
 int vry = A1;
 int xVal;
@@ -17,6 +19,7 @@ int x_speed;
 
 
 void setup() {
+  //we define motor driver outputs
   pinMode(button,INPUT);
 
   pinMode(in1,OUTPUT);
@@ -31,20 +34,22 @@ void setup() {
   digitalWrite(in2,LOW);
   digitalWrite(in3,LOW);
   digitalWrite(in4,LOW);
-  
+  //serial begin in case we would want to read data
   Serial.begin(9600);
 }
 
 void loop() {
-
+  
   xVal=analogRead(vrx);
   delay(100);
   yVal=analogRead(vry);
-  //b=digitalRead(button);
-
+  
+  //if we dont move the joystick, motors dont run
   if ((xVal>=490)&&(xVal<=530)){
     digitalWrite(ena,0);
   }
+  
+  //bigger or smaller values make motor run, from 0 to 255 progressively
   if (xVal>530){
     x_speed=map(xVal,550,1023,0,255);
     digitalWrite(in1,HIGH);
